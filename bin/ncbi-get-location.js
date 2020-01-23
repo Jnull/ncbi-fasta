360babf0385f641c3b814dbe22560fea9057f231
Null <admin@jarednull.com>
2020-01-22 20:43:39 -0800

    updated ncbi-get-location

--- bin/ncbi-get-location.js
+++ bin/ncbi-get-location.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
+const util = require('util');
 const fs = require('fs');
 const axios = require('axios');
 const parser = require('xml2json');
 
 let objectCount = 0;
 let searchResult;
-
+let resultObj;
+let xxx;
 // start with clear
-console.clear();
+//console.clear();
 
 
-// Configuration 
+// Configuration
 
 // first parameter is source file path
 let srcFilePath = process.argv[2];
@@ -27,12 +29,12 @@ initialize();
 
 function initialize() {
 	createDestFileFromNcbi(srcFilePath, destFilePath)
-	.then(result => {
-		//console.log('Result');
-		//console.log(result);
-		fs.writeFileSync(destFilePath, result);
-		console.log('complete');
-	});
+		.then(result => {
+			//console.log('Result');
+			//console.log(result);
+			fs.writeFileSync(destFilePath, result);
+			//console.log('complete');
+		});
 }
 
 async function createDestFileFromNcbi(sourceFilePath, destinationFilePath) {
@@ -51,32 +53,47 @@ async function createDestFileFromNcbi(sourceFilePath, destinationFilePath) {
 			let sourceRecord = sourceFileJsonArray[i];
 			let ncbiData = await fetchNcbiData(sourceRecord, (i + 1), sourceFileJsonArray.length);
 			let ncbiDataObj = await convertNcbiData(ncbiData);
+
+			//console.log(ncbiDataObj);
 			let mergedRecord = sourceRecord;
+			//console.log("harhahrhar", mergedRecord);
+
 			mergedRecord['country'] = ncbiDataObj.country;
+			mergedRecord['genus'] = ncbiDataObj.gName;
+			mergedRecord['species'] = ncbiDataObj.sName;
 			//console.log('record ' + i);
-			//console.log(mergedRecord);
+			// console.log(mergedRecord);
 			mergedRecordArray.push(mergedRecord);
 		}
 		let fastaResultString = "";
 		for(let i = 0; i < mergedRecordArray.length; i++){
 			let mergedRecord = mergedRecordArray[i];
+
+			let hasGenus = mergedRecord.genus && mergedRecord.genus.length > 0;
+			if(!hasGenus){ mergedRecord['genus'] = "" }
+
+			let hasSpecies = mergedRecord.species && mergedRecord.species.length > 0;
+			if(!hasSpecies){ mergedRecord['species'] = "" }
+
 			let hasCountry = mergedRecord.country && mergedRecord.country.length > 0;
-			if(!hasCountry){ mergedRecord['country'] = "" }
+			if(!hasCountry){ mergedRecord['country'] = "Location_Not_Found" }
+
 			let hasSequence = mergedRecord.sequence && mergedRecord.sequence.length > 0;
 			if(!hasSequence){ mergedRecord['sequence'] = "" }
-			fastaResultString += `>${mergedRecord.accessionNo}.${mergedRecord.version} ${mergedRecord.description} ${mergedRecord.country}\n${mergedRecord.sequence}\n`;
+
+			fastaResultString += `>${mergedRecord.accessionNo}.${mergedRecord.version} ${mergedRecord['genus']} ${mergedRecord['species']} ${mergedRecord.country}\n${mergedRecord.sequence}\n`;
 		}
 		return fastaResultString;
 	} catch(error) {
 		return error;
-	}	
+	}
 }
 
 async function readSourceFile(sourceFilePath) {
 	try {
 		// check if sourceFilePath is valid
 		if(!sourceFilePath){ throw new Error('Source File Path Not Provided') }
-		
+
 		// read source
 		return fs.readFileSync(sourceFilePath, 'utf8');
 
@@ -90,8 +107,9 @@ async function fastaToJson(fasta) {
 	try {
 		// check if sourceFilePath is valid
 		if(!fasta){ throw new Error('No Fasta Data Found In Source File') }
-		
+
 		// split by '>'
+		// console.log(fasta);
 		const fastaArray = fasta.split('>');
 
 		// remove first item in array which is always blank
@@ -101,12 +119,12 @@ async function fastaToJson(fasta) {
 		let fastaObjArray = [];
 
 		for(let i = 0; i < fastaArray.length; i++){
-			// set record 
+			// set record
 			let fastaRecord = fastaArray[i];
-			
+
 			// split record by space into array
 			let fastaRecordArray = fastaRecord.split(' ');
-			
+
 			// take the first item in the array which should be accessionNo.version,
 			// split by '.' into array, first item should be accessionNo,
 			// the second item should be the version number
@@ -143,19 +161,20 @@ async function fastaToJson(fasta) {
 
 	} catch(error) {
 		return error;
-	}	
+	}
 }
 
 async function fetchNcbiData(sourceRecord, recordNo, recordMaxNo){
 	try {
-		console.clear();
+		//console.clear();
 		let accession = `${sourceRecord.accessionNo}.${sourceRecord.version}`;
-		let url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=${accession}&rettype=json`;
-		console.log(`Record ${recordNo}/${recordMaxNo}:`);
-		console.log(`fetching ${url}`);
+		let url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=${accession}&rettype=json&complexity=3`;
+		//console.log(`Record ${recordNo}/${recordMaxNo}:`);
+		// console.log(`fetching ${url}`);
 		let responseXml = await axios.get(url);
 		let responseXmlData = responseXml.data;
 		let responseJsonObj = parser.toJson(responseXmlData, { object: true });
+
 		return responseJsonObj;
 	} catch(error) {
 		return error;
@@ -164,39 +183,63 @@ async function fetchNcbiData(sourceRecord, recordNo, recordMaxNo){
 
 async function convertNcbiData(ncbiData){
 	try {
+
 		await searchObject('SubSource', ncbiData);
 		let subSourceArray = searchResult;
 		searchResult = null;
+
+		await searchObject('BinomialOrgName_genus', ncbiData);
+		subSourceArray1 = searchResult;
+		searchResult = null;
+
+		await searchObject('BinomialOrgName_species', ncbiData);
+		subSourceArray2 = searchResult;
+		searchResult = null;
+
+
 		let country;
-		let resultObj = {
-			country: ""
+		resultObj = {
+			country: "",
+			gName: subSourceArray1, //genus name
+			sName: subSourceArray2  //species name
 		};
+
 		for(let i = 0; i < subSourceArray.length; i++){
 			let subSource = subSourceArray[i];
+			//console.log(subSource);
 			if(subSource['SubSource_subtype']['value'] === 'country'){
 				resultObj.country = subSource['SubSource_name'];
 			}
 		}
+
 		//console.log(resultObj);
 		return resultObj;
 	} catch(error) {
 		return error;
-	}	
+	}
 }
 
 async function searchObject(searchKey, obj){
+	//console.log(obj);
 	if(typeof obj === "object"){
+		//console.log(util.inspect(obj, {showHidden: false, depth: null}));
 		objectCount++;
 		//console.log(`Searching Object ${objectCount}`);
 		let objectKeys = Object.keys(obj);
+
 		for(let i = 0; i < objectKeys.length; i++){
+
 			let objectKey = objectKeys[i];
 			let result;
-			if(objectKey == searchKey){ 
-				//console.log(`found ${searchKey}`);
-				searchResult = obj[objectKey]; 
+
+			if(objectKey == searchKey){
+				//console.log(objectKey);
+				searchResult = obj[objectKey];
+				// console.log(searchResult);
 			}
+
 			if(searchResult){ return true; }
+
 			await searchObject(searchKey, obj[objectKey]);
 		}
 	}
